31240a566769bead41e3171c8d783d6688ff9b68
ekelly55 <eamonn.michael.kellyr@email.com>
2022-10-03 17:11:10 -0400

    detail page created, no data renders

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name='Home'),
     path('about/', views.About.as_view(), name='About'),
     path('pizzas/', views.PizzaList.as_view(), name = 'pizzas'),
-]
+    path('pizzas/new/', views.PizzaCreate.as_view(), name='pizza_create'),
+    path('pizzas/<int:pk>/', views.PizzaDetail.as_view(), name='pizza_detail'),
+] 
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from .models import Pizza
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
 # Create your views here.
 
 class Home(TemplateView):
@@ -24,3 +27,14 @@ class PizzaList(TemplateView):
             context['header'] = "Check out this za!"
         return context
 
+
+class PizzaCreate(CreateView):
+    model = Pizza
+    fields = ['image', 'name', 'review']
+    template_name = 'pizza_create.html'
+    success_url = '/pizzas/'
+
+
+class PizzaDetail(TemplateView):
+    model = Pizza
+    template_name = 'pizza_detail.html'
