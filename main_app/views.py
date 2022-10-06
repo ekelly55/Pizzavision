7c67f2bc2aa8f10540de6b877535527432414887
ekelly55 <eamonn.michael.kellyr@email.com>
2022-10-06 16:22:57 -0400

    routes protected

--- main_app/views.py
+++ main_app/views.py
@@ -9,10 +9,12 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
-
+@method_decorator(login_required, name = 'dispatch')
 class Home(TemplateView):
     template_name = 'home.html'
     def get_context_data(self, **kwargs):
@@ -27,6 +29,7 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = 'about.html'
 
+@method_decorator(login_required, name = 'dispatch')
 class PizzaList(TemplateView):
     template_name = 'pizza_list.html'
     def get_context_data(self, **kwargs):
@@ -40,13 +43,14 @@ class PizzaList(TemplateView):
             context['header'] = "Check out this za!"
         return context
 
-
+@method_decorator(login_required, name = 'dispatch')
 class PizzaCreate(CreateView):
     model = Pizza
     fields = ['image', 'name', 'review']
     template_name = 'pizza_create.html'
     success_url = '/pizzas/'
 
+@method_decorator(login_required, name = 'dispatch')
 class PizzaclubCreate(CreateView):
     model = Pizzaclub
     fields = ['club_name', 'characters']
@@ -57,6 +61,7 @@ class PizzaclubCreate(CreateView):
 
     success_url = '/'
 
+@method_decorator(login_required, name = 'dispatch')
 class PizzaUpdate(UpdateView):
     model = Pizza
     fields = ['image', 'name', 'review']
@@ -64,6 +69,7 @@ class PizzaUpdate(UpdateView):
     def get_success_url(self):
         return reverse('pizza_detail', kwargs= {'pk': self.object.pk})
 
+@method_decorator(login_required, name = 'dispatch')
 class PizzaDetail(DetailView):
     model = Pizza
     template_name = 'pizza_detail.html'
@@ -71,12 +77,13 @@ class PizzaDetail(DetailView):
         context = super().get_context_data(**kwargs)
         context['pizzaclubs'] = Pizzaclub.objects.all()
         return context
-
+@method_decorator(login_required, name = 'dispatch')
 class PizzaDelete(DeleteView):
     model = Pizza
     template_name = 'pizza_delete_confirmation.html'
     success_url = '/pizzas/'
 
+@method_decorator(login_required, name = 'dispatch')
 class CharacterCreate(View):
     def post(self, request, pk):
         name = request.POST.get('name')
@@ -85,6 +92,7 @@ class CharacterCreate(View):
         Character.objects.create(name=name, actor=actor, pizza=pizza)
         return redirect('pizza_detail', pk=pk)
 
+@method_decorator(login_required, name = 'dispatch')
 class PizzaclubCharacterAssoc(View):
     def get(self, request, pk, character_pk):
         assoc = request.GET.get('assoc')
